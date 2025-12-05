2e27c9074c9231c42b45486fcda8425bd0b44e0b
Pol <poljaimejuancaubet@gmail.com>
2025-12-05 16:15:28 +0100

    feat: auth stuff done

--- backend/app/auth/jwt_utils.py
+++ backend/app/auth/jwt_utils.py
@@ -1,16 +1,29 @@
 from datetime import datetime, timedelta, timezone
-from dotenv import load_dotenv
 from jose import jwt, JWTError
 from backend.app.config.config import settings as env
-from typing import Optional, Dict
+from typing import Optional
 
-load_dotenv()
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=env.ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc) + timedelta(
+        minutes=env.ACCESS_TOKEN_EXPIRE_MINUTES
+    )
     to_encode.update({"exp": expire, "token_type": "access"})
     return jwt.encode(to_encode, env.JWT_SECRET, algorithm=env.JWT_ALGORITHM)
+
+
+def create_access_tokenX(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, env.JWT_SECRET, algorithm=env.JWT_ALGORITHM)
+    return encoded_jwt
+
 
 def create_refresh_token(data: dict):
     to_encode = data.copy()
@@ -18,7 +31,8 @@ def create_refresh_token(data: dict):
     to_encode.update({"exp": expire, "token_type": "refresh"})
     return jwt.encode(to_encode, env.JWT_SECRET, algorithm=env.JWT_ALGORITHM)
 
-def decode_token(token: str) -> Optional[Dict]:
+
+def decode_token(token: str):
     try:
         user = jwt.decode(token, env.JWT_SECRET, algorithms=[env.JWT_ALGORITHM])
         return user
