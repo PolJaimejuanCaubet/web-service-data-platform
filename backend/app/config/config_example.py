3b2624c209bb05d90c748bcbede11a24468e31a8
Pol <poljaimejuancaubet@gmail.com>
2025-12-06 20:58:47 +0100

    chore: environmental variables

---
+++ backend/app/config/config_example.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    GOOGLE_API_KEY: str
+    STOCK_DATA: str
+    MONGO_URI: str
+    JWT_SECRET: str
+    JWT_ALGORITHM: str = "<the-encryption-algorithm-you-prefer>"
+    DB_NAME: str = "<database-name>"
+    DB_USER_COLLECTION: str = "<db-user-collection-name>"
+    DB_STOCKS_COLLECTION: str = "<db-stocks-collection-name>"
+    DB_ETL_LOGS_COLLECTION: str = "<db-etl-logs-collection-name>"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
