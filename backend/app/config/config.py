8bc3e892c183d00fe6946b568b376461ab5fff32
Pol <poljaimejuancaubet@gmail.com>
2025-12-11 17:12:57 +0100

    config file

---
+++ backend/app/config/config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    GOOGLE_API_KEY: str
+    STOCK_DATA: str
+    MONGO_URI: str
+    JWT_SECRET: str
+    JWT_ALGORITHM: str
+    DB_NAME: str
+    DB_USER_COLLECTION: str
+    DB_STOCKS_COLLECTION: str
+    DB_ETL_LOGS_COLLECTION: str
+    DB_HISTORY_COLLECTION: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    
+    
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
